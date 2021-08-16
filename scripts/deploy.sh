861e7595e7087b4474ee3b08f08323e4e93058f1
Will Usher <wusher@kth.se>
2021-08-16 09:43:05 +0200

    Script deployment of scorm packages

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+for lecture in _build/lecture_*; do
+
+    OUTPUT=_deploy/$(basename $lecture)
+    NUMBER=`echo ${lecture:(-2)}`
+
+    echo $OUTPUT $lecture $NUMBER
+    scorm $OUTPUT $lecture $NUMBER
+done;
